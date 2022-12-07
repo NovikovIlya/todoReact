b211fc6f4dc386e317371a13818eba017738816e
NovikovIlya <novikov.i.sergeevich@gmail.com>
2022-12-07 13:25:31 +0300

    3

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function Result({correct}) {
   return (
     <div className="result">
       <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
-      <h2>Вы отгадали {correct} ответа из {questions.length}</h2>
+      <h2>Вы отгадали {correct} ответа из {questions.length}Вы молодец!</h2>
       <button onClick={reload}>Попробовать снова</button>
     </div>
   );
